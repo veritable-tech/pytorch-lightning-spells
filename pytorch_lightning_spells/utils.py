a16fe806a7372886062e8973521e94b8570d9145
Ceshine Lee <shuanck@gmail.com>
2021-01-11 06:58:10 +0800

    Avoid taking in NaNs

--- pytorch_lightning_spells/utils.py
+++ pytorch_lightning_spells/utils.py
@@ -1,3 +1,8 @@
+import warnings
+
+import torch
+
+
 class EMATracker:
     """Keeps the exponential moving average for a single series.
 
@@ -5,15 +10,18 @@ class EMATracker:
     ----------
     alpha : float, optional
         the weight of the new value, by default 0.05
-    """       
+    """
 
-    def __init__(self, alpha: float = 0.05):     
+    def __init__(self, alpha: float = 0.05):
         super().__init__()
         self.alpha = alpha
         self._value = None
 
     def update(self, new_value):
         new_value = new_value.detach()
+        if torch.isnan(new_value):
+            warnings.warn("NaN encountered as training loss!")
+            return
         if self._value is None:
             self._value = new_value
         else:
