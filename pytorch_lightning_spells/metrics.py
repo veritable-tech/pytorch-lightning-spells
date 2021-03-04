1f0b9dab0bd5d3a99e56e49e118725744e3b236e
Ceshine Lee <shuanck@gmail.com>
2021-03-04 20:54:58 +0800

    Add Spearman Correlation

--- pytorch_lightning_spells/metrics.py
+++ pytorch_lightning_spells/metrics.py
@@ -3,6 +3,7 @@ from typing import Optional, Any
 
 import torch
 import numpy as np
+from scipy.stats import spearmanr
 from pytorch_lightning.metrics import Metric
 from pytorch_lightning.utilities import rank_zero_warn
 from sklearn.metrics import fbeta_score, roc_auc_score
@@ -51,6 +52,35 @@ class AUC(GlobalMetric):
         if len(np.unique(target)) == 1:
             return torch.tensor(0, device=self.preds[0].device)
         return torch.tensor(roc_auc_score(target, preds), device=self.preds[0].device)
+
+
+class SpearmanCorrelation(GlobalMetric):
+    def __init__(
+        self,
+        sigmoid: bool = False,
+        compute_on_step: bool = False,
+        dist_sync_on_step: bool = False,
+        process_group: Optional[Any] = None,
+    ):
+        super().__init__(
+            compute_on_step=compute_on_step,
+            dist_sync_on_step=dist_sync_on_step,
+            process_group=process_group,
+        )
+        self.sigmoid = sigmoid
+
+    def compute(self):
+        preds = torch.cat(self.preds, dim=0).float()
+        if self.sigmoid:
+            preds = torch.sigmoid(preds)
+        preds = preds.cpu().numpy()
+        target = torch.cat(self.target, dim=0).cpu().float().numpy()
+        spearman_score = spearmanr(
+            target, preds
+        ).correlation
+        if len(np.unique(target)) == 1:
+            return torch.tensor(0, device=self.preds[0].device)
+        return torch.tensor(spearman_score, device=self.preds[0].device)
 
 
 class FBeta(GlobalMetric):
