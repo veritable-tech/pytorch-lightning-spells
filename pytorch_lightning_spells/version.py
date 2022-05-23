2533745943d5ba4e6797fe105f384ad5284a5de4
Ceshine Lee <shuanck@gmail.com>
2022-05-23 22:48:25 +0800

    Cleaning up version.py

--- pytorch_lightning_spells/version.py
+++ pytorch_lightning_spells/version.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-__version__ = "0.0.5"
-=======
 __version__ = "0.1.0"
->>>>>>> pl_1_5
 __docs__ = "Some useful plugins for PyTorch Lightning."
 __author__ = "Ceshine Lee"
 __author_email__ = "ceshine@veritable.pw"
