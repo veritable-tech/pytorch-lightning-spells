8369ef69d66787b73722cbc31bccc00c40b534a9
Ceshine Lee <shuanck@gmail.com>
2024-02-16 20:52:11 +0800

    Relax the requirements of the PyTorch Lightning version

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ try:
 except (ImportError, ModuleNotFoundError):
     # alternative https://stackoverflow.com/a/67692/4521646
     import sys
+
     sys.path.append("pytorch_lightning_spells")
     import version as pls
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
@@ -21,36 +22,43 @@ setup(
     description=pls.__docs__,
     author=pls.__author__,
     author_email=pls.__author_email__,
-    url='https://github.com/veritable-tech/pytorch-lightning-spells',
-    download_url='https://github.com/veritable-tech/pytorch-lightning-spells',
+    url="https://github.com/veritable-tech/pytorch-lightning-spells",
+    download_url="https://github.com/veritable-tech/pytorch-lightning-spells",
     license=pls.__license__,
-    packages=find_packages(exclude=['tests', 'tests/*', 'benchmarks']),
+    packages=find_packages(exclude=["tests", "tests/*", "benchmarks"]),
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords=['deep learning', 'pytorch', 'AI'],
-    python_requires='>=3.8',
+    long_description_content_type="text/markdown",
+    keywords=["deep learning", "pytorch", "AI"],
+    python_requires=">=3.8",
     setup_requires=[],
-    install_requires=["pytorch_lightning>=2.0.0, <2.1.0", "torchmetrics>=0.11.0, <0.12.0", "scipy", "scikit-learn"],
+    install_requires=[
+        "pytorch_lightning>=2.0.0, <2.2.0",
+        "torchmetrics>=0.11.0, <0.15.0",
+        "scipy",
+        "scikit-learn",
+    ],
     extras_require={},
     classifiers=[
-        'Environment :: Console',
-        'Natural Language :: English',
+        "Environment :: Console",
+        "Natural Language :: English",
         # How mature is this project? Common values are
         #   3 - Alpha, 4 - Beta, 5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        "Development Status :: 4 - Beta",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Scientific/Engineering :: Image Recognition',
-        'Topic :: Scientific/Engineering :: Information Analysis',
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Information Analysis",
         # Pick your license as you wish
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
